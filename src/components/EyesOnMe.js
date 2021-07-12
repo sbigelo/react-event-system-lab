2a8ca33374c8ef8a961ef2a37c4dea97b354d6a3
sbigelo <sean.bigelow1@gmail.com>
2021-07-12 12:21:42 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,31 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+
+
+class EyesOnMe extends React.Component {
+
+
+    blurr = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    focuss = () => {
+        console.log('Good!')
+    }
+
+    render() {
+        return(
+            <div>
+                <button 
+                onFocus={this.focuss}
+                onBlur={this.blurr}
+                >
+                </button>
+            </div>
+        )
+    }
+
+
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
-// Code Keypad Component Here
+import React from "react";
+
+
+class Keypad extends React.Component {
+
+
+    enterPassword = () => {
+        console.log('Entering password...')
+    }
+
+
+    render() {
+        return(
+            <div>
+            <input onKeyUp={this.enterPassword} type="password" />    
+            </div>
+        )
+    }
+
+
+}
+
+export default Keypad;
